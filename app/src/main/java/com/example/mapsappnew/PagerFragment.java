2a9d69b40ce9ee0b6d884654fdf23671fccfc492
Ellis2010 <72437997+Ellis2010@users.noreply.github.com>
2023-03-11 17:30:17 +0000

    main_screen_implementation
    Began on creating functionality for main screen on the application.

---
+++ app/src/main/java/com/example/mapsappnew/PagerFragment.java
@@ -0,0 +1,4 @@
+package com.example.mapsappnew;
+
+public class PagerFragment {
+}
---
+++ app/src/main/java/com/example/mapsappnew/SwipeFragment.java
@@ -0,0 +1,17 @@
+package com.example.mapsappnew;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class SwipeFragment extends Fragment {
+
+    public static SwipeFragment newInstance(String text) {
+
+        //
+        Bundle bundle = new Bundle();
+
+        return new SwipeFragment();
+    }
+
+}
